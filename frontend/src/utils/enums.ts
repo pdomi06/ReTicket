e6480f8054a5e783142f99728a4adb394354ecba
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2026-02-11 23:31:21 +0100

    Added enums
    Made a file in the utils for enums and filled it out.

---
+++ frontend/src/utils/enums.ts
@@ -0,0 +1,61 @@
+// User - kycStatus
+export const KycStatus = {
+  Pending: "pending",
+  Rejected: "rejected",
+  Approved: "approved",
+} as const;
+
+// Events - category
+export const EventCategory = {
+  Cultural: "cultural",
+  Music: "music",
+  Sport: "sport",
+} as const;
+
+// Original Tickets - status
+export const TicketStatus = {
+  PreRelease: "pre-release",
+  Active: "active",
+  Cancelled: "cancelled",
+  Expired: "expired",
+} as const;
+
+// Orders - status
+export const OrderStatus = {
+  Pending: "pending",
+  Processing: "processing",
+  Completed: "completed",
+  Failed: "failed",
+  Refunded: "refunded",
+} as const;
+
+// Orders - paymentStatus
+export const PaymentStatus = {
+  Pending: "pending",
+  Authorized: "authorized",
+  Captured: "captured",
+  Failed: "failed",
+  Refunded: "refunded",
+} as const;
+
+// Orders - deliverStatus
+export const DeliveryStatus = {
+  Pending: "pending",
+  Sent: "sent",
+  Delivered: "delivered",
+} as const;
+
+// Payouts - status
+export const PayoutStatus = {
+  Created: "created",
+  Pending: "pending",
+  Cancelled: "cancelled",
+  Fulfilled: "fulfilled",
+} as const;
+
+// User Settings - profileVisibility
+export const ProfileVisibility = {
+  Visible: "visible",
+  Restricted: "restricted",
+  Banned: "banned",
+} as const;
