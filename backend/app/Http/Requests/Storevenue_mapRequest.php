b48b15be78c5e7809d803eabb2d183613005ae10
Ati <molnarattila585@gmail.com>
2026-02-12 15:48:32 +0100

    -
    .

--- backend/app/Http/Requests/Storevenue_mapRequest.php
+++ backend/app/Http/Requests/Storevenue_mapRequest.php
@@ -11,7 +11,7 @@ class Storevenue_mapRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ActiveTicketsController;
+use App\Http\Controllers\VenueMapController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,8 +9,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get("/activeTickets", [ActiveTicketsController::class,"index"]);
-Route::get("/activeTickets/{id}", [ActiveTicketsController::class,"show"]);
-Route::post("/activeTickets", [ActiveTicketsController::class,"store"]);
-Route::put("/activeTickets/{id}", [ActiveTicketsController::class,"update"]);
-Route::delete("/activeTickets/{id}", [ActiveTicketsController::class,"destroy"]);
+
+Route::apiResource("activeTickets", ActiveTicketsController::class);
+
+Route::apiResource("venue", VenueMapController::class);
