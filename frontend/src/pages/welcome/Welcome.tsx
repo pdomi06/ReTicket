e5390a4bd75d84e5e685528068a1808da9f30ca9
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2026-02-17 01:28:29 +0100

    filled in mock data for visualization

--- frontend/src/pages/welcome/Welcome.tsx
+++ frontend/src/pages/welcome/Welcome.tsx
@@ -7,14 +7,33 @@ import mockPicture from '../../../public/img/mock/music_genre.png';
 const Welcome = () => {
     return (
         <main className={`${style['welcome-container']} container`}>
-            <Cards>
-            <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture}/>
-            <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture}/>
-            <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture}/>
-            <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture}/>
-            <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture}/>
-            <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture}/>
-            </Cards>
+            {/* TODO: Loading cards from api.*/}
+            <div className={`${style['upcomming-events-container']} container-fluid my-5`}>
+                <h1 className={style['events-title']}>Upcomming Events</h1>
+                <h6 className={style['events-subtitle']}>Trending events in the near future</h6>
+                <Cards>
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                </Cards>
+            </div>
+            <div className={`${style['upcomming-events-container']} container-fluid my-5`}>
+                <h1 className={style['events-title']}>Upcomming Events</h1>
+                <h6 className={style['events-subtitle']}>Trending events in the near future</h6>
+                <Cards>
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                    <Card title="Brodway" description="Excelentshow" imageUrl={mockPicture} />
+                </Cards>
+            </div>
         </main>
     )
 }
