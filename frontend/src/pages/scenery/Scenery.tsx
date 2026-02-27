f31d43fa1168e14d99c8e937d89a5ab08acf4cf7
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2026-02-27 18:20:39 +0100

    redesigned scenery

--- frontend/src/pages/scenery/Scenery.tsx
+++ frontend/src/pages/scenery/Scenery.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import type { ISceneryMap } from "../../utils/interfaces";
 import Input from "../../components/ui/input/Input";
 import style from './Scenery.module.css'
+import Button from "../../components/ui/button/Button";
 
 const Scenery = () => {
     const [sceneryParams, setSceneryParams] = useState<ISceneryMap | null>(null);
@@ -9,15 +10,19 @@ const Scenery = () => {
         console.log(sceneryParams);
     }
     return (
-        <div className={style["scenery-main-div"]}>
-            <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }} className={style["scenery-form"]}>
-                <h1>Scenery Designer</h1>
-                <hr />
-                <Input type="text" name="name" label="Name" onChange={(e) => setSceneryParams({ ...sceneryParams, name: e.target.value } as ISceneryMap)} />
-                <Input type="number" name="width" label="Width" onChange={(e) => setSceneryParams({ ...sceneryParams, width: Number(e.target.value) } as ISceneryMap)} />
-                <Input type="number" name="height" label="Height" onChange={(e) => setSceneryParams({ ...sceneryParams, height: Number(e.target.value) } as ISceneryMap)} />
-                <Input type="number" name="rate" label="Rate" step={0.01} onChange={(e) => setSceneryParams({ ...sceneryParams, rate: Number(e.target.value) } as ISceneryMap)} />
-                <button type="submit">Generate Scenery</button>
+        <div className="container my-5">
+            <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }} className={`container w-50`}>
+                <div className={`rounded p-4 ${style['scenery-form']}`}>
+                    <h1>Scenery Designer</h1>
+                    <hr />
+                    <div className="d-grid gap-3">
+                        <Input type="text" name="name" label="Name" onChange={(e) => setSceneryParams({ ...sceneryParams, name: e.target.value } as ISceneryMap)} />
+                        <Input type="number" name="width" label="Width" onChange={(e) => setSceneryParams({ ...sceneryParams, width: Number(e.target.value) } as ISceneryMap)} />
+                        <Input type="number" name="height" label="Height" onChange={(e) => setSceneryParams({ ...sceneryParams, height: Number(e.target.value) } as ISceneryMap)} />
+                        <Input type="number" name="rate" label="Rate" step={0.01} onChange={(e) => setSceneryParams({ ...sceneryParams, rate: Number(e.target.value) } as ISceneryMap)} />
+                        <Button type="submit" text="Create Scenery" />
+                    </div>
+                </div>
             </form>
         </div>
     )
