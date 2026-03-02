d57523da55e63d441a86ae46443071aa61e4c96d
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2026-03-02 18:00:18 +0100

    Update 2026_01_23_122219_create_venue_maps_table.php

--- backend/database/migrations/2026_01_23_122219_create_venue_maps_table.php
+++ backend/database/migrations/2026_01_23_122219_create_venue_maps_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->text('section');
             $table->unsignedSmallInteger('rows');
             $table->unsignedSmallInteger('cols');
-            $table->decimal('rate', 8, 2);
+            $table->decimal('rate', 1, 2);
         });
     }
 
