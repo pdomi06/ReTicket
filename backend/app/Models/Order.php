eae99a620348573f5eab424b5a74cf5533cc9030
Ati <molnarattila585@gmail.com>
2026-03-03 22:00:07 +0100

    order model
    Added eloquent model relationships to Order model

--- backend/app/Models/Order.php
+++ backend/app/Models/Order.php
@@ -11,6 +11,15 @@ class Order extends Model
     use HasFactory;
 
     protected $table = "orders";
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems(){
+        return $this->hasMany(OrderItem::class);
+    }
+
     protected $fillable = [
         'orderNumber',
         'buyerEmail',
