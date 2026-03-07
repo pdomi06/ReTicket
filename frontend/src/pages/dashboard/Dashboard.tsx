7c8e5a11aaa7f1304adc601bfe2bf4ef819794fe
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2026-03-07 19:51:00 +0100

    removed css

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,11 @@
-import styles from './Dashboard.module.css';
 
 const Dashboard = () => {
     return (
-        <div className={`row ${styles.dashboard}`}>
-            <div className={`col-3 ${styles.sidebar}`}>
+        <div className={`row`}>
+            <div className={`col-3`}>
                 <h2>Dashboard Menu</h2>
             </div>
-            <div className={`col-9 ${styles.content}`}>
+            <div className={`col-9`}>
                 <h1>Dashboard</h1>
                 <p>Welcome to your dashboard! Here you can manage your events, view sales, and access analytics.</p>
                 <p>Use the navigation menu to access different sections of your dashboard.</p>
