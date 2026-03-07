b59e7e9952ec5884778090a8931766cd0d60c97c
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2026-03-07 19:51:48 +0100

    Update Dashboard.tsx

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -2,10 +2,10 @@
 const Dashboard = () => {
     return (
         <div className={`row`}>
-            <div className={`col-3`}>
+            <div className={`col-12 col-lg-3`}>
                 <h2>Dashboard Menu</h2>
             </div>
-            <div className={`col-9`}>
+            <div className={`col-12 col-lg-9`}>
                 <h1>Dashboard</h1>
                 <p>Welcome to your dashboard! Here you can manage your events, view sales, and access analytics.</p>
                 <p>Use the navigation menu to access different sections of your dashboard.</p>
