92135ba82a5d22b8188e8e35b648a57152d08e22
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2026-03-08 15:55:14 +0100

    Update useEventData.ts

--- frontend/src/pages/event/useEventData.ts
+++ frontend/src/pages/event/useEventData.ts
@@ -79,7 +79,7 @@ export function useEventData(eventId: string) {
         return () => {
             cancelled = true
         }
-    }, [eventId])
+    }, [eventId, getEvent])
 
     useEffect(() => {
         if (!event?.name) {
