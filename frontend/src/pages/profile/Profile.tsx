205c2b35cffe61eab82399d534f65a9ddb11dbed
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2026-03-19 16:23:53 +0100

    Removed 0Auth for auth:sanctum

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -1,23 +1,8 @@
-import { useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { user, isAuthenticated, loginWithRedirect } = useAuth0();
-  console.log(user);
-  useEffect(() => {
-    if (!isAuthenticated) {
-      loginWithRedirect();
-    }
-  }, [isAuthenticated, loginWithRedirect]);
-
-  if (!isAuthenticated) {
-    return <div>Redirecting to login...</div>;
-  }
   return (
     <div>
-        {user?.name}
-        {user?.email}
-        <img src={user?.picture} alt={user?.name} />
+      <h1>Profile Page</h1>
     </div>
   );
 }
